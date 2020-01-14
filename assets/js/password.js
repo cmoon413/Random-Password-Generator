f690a850af25f33748a444f36ff4b439aa4c9c08
Chester Moon <cmoon3740@gmail.com>
2020-01-13 19:26:40 -0800

    changed method for password to a prefilled array to check against

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,21 +1,29 @@
-const min = 33;
-const max = 126;
-let passwordSize = 8;
-let AsciiPassword = [];
+const min = 33
+const max = 126
+let passwordSize
+let AsciiPassword = []
+var charMap = []
+
+//creating a array of possible characters to run functions against
+for (let i = 33; i < 127; i++) {
+    charMap.push(String.fromCharCode(i))
+
+}
 
 //creates a random number based on ascii value range
-function getRandom  ()  {
-    return Math.random() * (max - min) + min;
+function getRandom() {
+    return Math.random() * 93
 };
 
 
 //fills an array with random ascii values based on passwordsize
-function CreatePasswordArray    (size)  {       
-    for(let i=0; i<size; i++)   {
-        AsciiPassword.push("&#" + parseInt(getRandom()));
+function CreatePasswordArray(size) {
+    for (let i = 0; i < size; i++) {
+        AsciiPassword.push(charMap[parseInt(getRandom())]);
     }
 
 };
 
-CreatePasswordArray(passwordSize);
-document.getElementById('demo').innerHTML = "<p>" + AsciiPassword[0] + "</p>" 
+passwordSize = prompt('How many characters is your password?')
+CreatePasswordArray(passwordSize)
+alert(AsciiPassword)
