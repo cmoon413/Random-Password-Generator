21220d50a41fb6f3302575d520f969600f1a75fa
Chester Moon <cmoon3740@gmail.com>
2020-01-15 19:34:12 -0800

    modified random number generator to except input for array length

--- assets/js/password.js
+++ assets/js/password.js
@@ -2,7 +2,11 @@ const min = 33
 const max = 126
 let passwordSize
 let AsciiPassword = []
-var charMap = []
+let charMap = []
+let specialChar
+let numeric
+let upperCase
+let lowerCase
 
 //creating a array of possible characters to run functions against
 for (let i = 33; i < 127; i++) {
@@ -10,20 +14,27 @@ for (let i = 33; i < 127; i++) {
 
 }
 
-//creates a random number based on ascii value range
-function getRandom() {
-    return Math.random() * 93
-};
+//creates a random number
+function getRandom(arrayLength) {
+    return Math.random() * (arrayLength + 1)
+}
 
 
 //fills an array with random ascii values based on passwordsize
 function CreatePasswordArray(size) {
     for (let i = 0; i < size; i++) {
-        AsciiPassword.push(charMap[parseInt(getRandom())]);
+        AsciiPassword.push(charMap[parseInt(getRandom(charMap.length))]);
     }
 
-};
+}
 
 passwordSize = prompt('How many characters is your password?')
-CreatePasswordArray(passwordSize)
-alert(AsciiPassword)
+specialChar = confirm('would you like special characters in it?')
+numeric = confirm('would you like numbers in it?')
+upperCase = confirm('would you like uppercase letters?')
+lowerCase = confirm('would you like lowercase in the password?')
+
+if (specialChar === true && numeric === true & upperCase === true && lowerCase === true) {
+    CreatePasswordArray(passwordSize);
+    alert(AsciiPassword);
+}
